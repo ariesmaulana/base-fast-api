0b7809f7572c93d8469607a19d274b2dda11030b
ariesm <ariesmpernama@gmail.com>
2025-07-20 15:27:39 +0700

    feat: fix ci

--- app/settings.py
+++ app/settings.py
@@ -36,7 +36,6 @@ class Settings(BaseSettings):
     R2_BUCKET_NAME: str = Field(..., env="R2_BUCKET_NAME")
     R2_PUBLIC_BASE_URL: str = Field(..., env="R2_PUBLIC_BASE_URL")
     R2_REGION: str = Field(..., env="R2_REGION")
-    GEMINI_API_KEY: str = Field(..., env="GEMINI_API_KEY")
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
